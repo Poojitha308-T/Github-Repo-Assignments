9c817ccbeadbdf4a4b2f751dccff8257409d7205
Poojitha308-T <poojithathadiboyina@gmail.com>
2025-11-14 16:06:54 +0530

    EvenOdd

---
+++ Day_5/evenodd.js
@@ -0,0 +1,12 @@
+function EvenOdd(num){
+    for(let i=1;i<=num;i++)
+    {
+        if(num%2===0)
+        {
+            return true
+        }
+        return false
+    }
+}
+console.log(EvenOdd(17))
+console.log(EvenOdd(246))
