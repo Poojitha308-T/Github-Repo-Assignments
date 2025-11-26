ccfb9c78600e5b73af0d83b96256443af4ac798e
Poojitha308-T <poojithathadiboyina@gmail.com>
2025-11-26 22:46:05 +0530

    Age

---
+++ Day_15/Q1/Q19.js
@@ -0,0 +1,19 @@
+var age = 10;
+
+function displayAge() {
+    console.log("display age", age);
+}
+
+function changeAge() {
+    age++;
+    console.log("change age", age);
+}
+
+displayAge();
+changeAge();
+
+displayAge();
+changeAge();
+
+displayAge();
+changeAge();
