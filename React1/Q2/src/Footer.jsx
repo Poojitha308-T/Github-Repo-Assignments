d76822805329edee044ded19bc0f3d295bc9b53f
Poojitha308-T <poojithathadiboyina@gmail.com>
2025-12-15 16:40:55 +0530

    Q2

---
+++ React1/Q2/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div style={{ color: "black", fontSize: "15px", textAlign: "center"}}> This is Footer </div >
+  )
+}
+
+export default Footer
---
+++ React1/Q2/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div style={{backgroundColor: "aqua", color:"red", padding:"20px"}}>
+        <p>This is Navbar</p>
+    </div>
+  )
+}
+
+export default Navbar
