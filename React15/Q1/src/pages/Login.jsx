afcd03010278013a4cf0d8fca6ea05a8fbdcb787
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-07 23:38:50 +0530

    React Todos App

--- React15/Q1/src/pages/Login.jsx
+++ React15/Q1/src/pages/Login.jsx
@@ -1,43 +1,27 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const { login } = useAuth();
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const submit = async () => {
-    await login(email, password);
-    navigate("/todos");
-  };
+export default function Login() {
+const { login } = useAuth();
+const navigate = useNavigate();
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
 
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="w-90 space-y-4">
-        <div>Email : 
-        <input
-        type="email"
-          placeholder="Enter your Email"
-          onChange={(e) => setEmail(e.target.value)}
-          class="border-2 rounded px-2 ml-2"
-        />
-        </div>
-        <div>Password : 
-        <input
-          type="password"
-          placeholder="Enter your Password"
-          onChange={(e) => setPassword(e.target.value)}
-          class="border-2 rounded px-2 ml-2"
-        />
-        </div>
-        <div>
-          <button onClick={submit} className="btn-primary">Login</button>
-        </div>
-      </div>
-    </div>
-  );
+
+const submit = async e => {
+e.preventDefault();
+await login(email, password);
+navigate("/todos");
 };
 
-export default Login;
+
+return (
+<form onSubmit={submit} className="h-screen flex flex-col justify-center items-center gap-4">
+<input onChange={e => setEmail(e.target.value)} placeholder="Email" />
+<input type="password" onChange={e => setPassword(e.target.value)} placeholder="Password" />
+<button>Login</button>
+</form>
+);
+}
