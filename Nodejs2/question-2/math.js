4b2b02df57ee589b6737472b5fc609a01808b7c9
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-13 23:36:13 +0530

    Custom Module

---
+++ Nodejs2/question-2/math.js
@@ -0,0 +1,13 @@
+function checkPrime(number){
+    if(number<=1) return false;
+    if(number === 2) return true;
+
+    for(let i=2;i<=Math.sqrt(number);i++)
+    {
+        if(number%i === 0){
+            return false;
+        } 
+    }
+    return true;
+}
+export default checkPrime;
